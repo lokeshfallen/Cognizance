601c50785b6925f98e8dc2074c7716df5a253049
lokeshfallen <ch.en.u4cce21029@ch.students.amrita.edu>
2022-02-15 11:17:47 +0530

    Program to add three numbers

---
+++ Task-1/Sum_of_3.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+
+    int num_1, num_2, num_3;
+
+    printf("Enter the first number :");
+    scanf("%d", &num_1);
+
+    printf("Enter the second number :");
+    scanf("%d", &num_2);
+
+    printf("Enter the third number :");
+    scanf("%d", &num_3);
+
+    printf("the sum : %d", num_1 + num_2 + num_3);
+}
