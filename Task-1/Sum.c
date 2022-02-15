9055a507b652531588f5da549d11fb3ebaa89af6
lokeshfallen <ch.en.u4cce21029@ch.students.amrita.edu>
2022-02-15 11:02:38 +0530

    Program to add two numbers

---
+++ Task-1/Sum.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+
+    int num_1, num_2;
+    printf("Enter the first number : ");
+    scanf("%d", &num_1);
+
+    printf("Enter the second number : ");
+    scanf("%d", &num_2);
+
+    printf("The sum : %d", num_1 + num_2);
+
+}
